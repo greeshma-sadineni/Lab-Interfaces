66f4ac01de79d3ed1fc7455ff436254560849912
Rajesh Bathula <greeshma-sadineni.com>
2020-05-22 13:05:43 +0200

    first commit

---
+++ Lab Interfaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius:");
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+            Console.WriteLine("Enter width: ");
+            var width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height: ");
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height); 
+           
+
+
+           
+          
+            circle.Draw();
+            rect.Draw();
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab Interfaces/Rectangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Lab_Interfaces
+{
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int width,int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+           private set
+            {
+                this.width = value;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                this.height = value;
+            }
+        }
+        public void Draw()
+        {
+            DrawLine(this.Width,'*','*');
+            for (int i = 1; i<this.Height-1; i++)
+            {
+                DrawLine(this.Width,' ','*');
+            }
+
+            DrawLine(this.Width,'*','*');
+        }
+
+
+        public void DrawLine(int width, char end, char mid)
+        {
+            Console.WriteLine(end);
+            for (int i = 1; i<width - 1; i++)
+            {
+                Console.WriteLine(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
